517dc06bc3a2cc9adba3b0854d6bcf0bcc5a8a8e
Daniel bezerra <danielbezerrakmx@gmail.com>
2019-02-06 12:42:56 -0200

    testing listFavoritePokemon

--- src/tests/components/ListFavoritePokemon.test.js
+++ src/tests/components/ListFavoritePokemon.test.js
@@ -8,43 +8,13 @@ import ListFavoritePokemon from "../../components/ListFavoritePokemon";
 const mockStore = configureMockStore();
 
 describe("ListFavoritePokemon", () => {
-    const mockPokemon =  {
-        sprites: {
-            back_default:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png"
-          },
-          name: "ditto",
-          abilities: [
-            {
-              ability: {
-                name: "imposter",
-                url: 'https://pokeapi.co/api/v2/ability/150/'
-              }
-            },
-            {
-              ability: {
-                name: "imposter",
-                url: "https://pokeapi.co/api/v2/ability/151/"
-              }
-            }
-          ],
-          types: [
-            {
-              type: {
-                name: "normal",
-                url: "https://pokeapi.co/api/v2/ability/151/"
-              }
-            }
-          ]
-        
-      }
 const store = mockStore({pokemons: [mockPokemon]});
 
     it("should mount app", () => {
         console.log(store)
        mount(
         <Provider store={store}>
-          <ListFavoritePokemon pokemons={[mockPokemon]} />
+          <ListFavoritePokemon />
         </Provider>
       );
     });
