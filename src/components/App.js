08f0704302f40c673623db72410e401700295164
Daniel bezerra <danielbezerrakmx@gmail.com>
2019-02-05 22:19:21 -0200

    removendo setState

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,11 @@ import Pokeapi from "../apis/Pokeapi";
 import ListPokemon from "./ListPokemon";
 
 class App extends React.Component {
-  state = { pokemonData: {} };
-  onSearched = async term => {
-    const results = await Pokeapi.get("/pokemon/" + term);
-    this.setState({ pokemonData: results.data });
-  };
   render() {
     return (
       <div>
-        <SearchBar onSubmit={e => this.onSearched(e)} />
-        <ListPokemon pokemon={this.state.pokemonData} />
+        <SearchBar/>
+        <ListPokemon />
       </div>
     );
   }
